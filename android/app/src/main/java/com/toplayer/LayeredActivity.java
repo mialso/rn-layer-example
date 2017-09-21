ce5d9af290bce0a15a25e03b71df55b63944f8b7
Nick Panfilov <pnickv@gmail.com>
2017-09-21 23:51:02 +0300

    Added some logic to intercept & check touch events behind React Native Layer

--- android/app/src/main/java/com/toplayer/LayeredActivity.java
+++ android/app/src/main/java/com/toplayer/LayeredActivity.java
@@ -1,16 +1,14 @@
 package com.toplayer;
 
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ViewGroup;
-
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.hudl.oss.react.fragment.ReactFragment;
 
 public class LayeredActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
 
+    BaseFragment b1, b2;
     private static final String REACT_COMP_NAME = "topLayer";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +16,19 @@ public class LayeredActivity extends AppCompatActivity implements DefaultHardwar
         setContentView(R.layout.layered_activity);
 
         if (savedInstanceState == null) {
-            ReactFragment reactFragment = new ReactFragment.Builder(REACT_COMP_NAME).build();
+            getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.layer_container, b1 = new BaseFragment())
+                .commit();
+
+            getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.layer_container, b2 = new BaseFragment())
+                .commit();
 
             getSupportFragmentManager()
                     .beginTransaction()
-                    .add(R.id.layer_container, reactFragment)
+                    .add(R.id.layer_container, new ReactFragment.Builder(REACT_COMP_NAME).build())
                     .commit();
 
         }
@@ -34,4 +40,13 @@ public class LayeredActivity extends AppCompatActivity implements DefaultHardwar
         super.onBackPressed();
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (!b2.dispatchTouchEvent(ev)) {
+            if (!b1.dispatchTouchEvent(ev)) {
+                return super.dispatchTouchEvent(ev);
+            }
+        }
+        return true;
+    }
 }
